32be3a9fb2e8e545d9a3fe3e761017bcc31677d2
Milan Mydlarčík <milanmydlarcik@inove.cz>
2022-09-08 21:30:49 +0200

    [WiP] Create EmployeeControler with view, fetch data

---
+++ app/Models/Employee.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['id', 'jmeno', 'prijmeni', 'date'];
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->jmeno;
+    }
+
+    public function getSurname()
+    {
+        return $this->prijmeni;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
---
+++ resources/js/app.js
@@ -0,0 +1,18 @@
+import './bootstrap';
+
+/* fetch('https://xevos.store/domaci-ukol/Jmena.json', {
+
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+
+    .then(res => res.json())
+
+    .then((result) => {
+        console.log(result);
+    }, (error) => {
+        alert('Failed');
+    }) */
